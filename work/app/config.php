1d50f32160b0f731f55a218a55b8550eceae4820
yaguruma <yaguruma_y@yahoo.co.jp>
2021-10-17 13:15:18 +0900

    クラスをオートリロードに変更

--- work/app/config.php
+++ work/app/config.php
@@ -7,8 +7,17 @@ define('DB_USER', 'myappuser');
 define('DB_PASS', 'myapppass');
 define('SITE_URL', 'http://' . $_SERVER['HTTP_HOST']);
 
-require_once(__DIR__ . '/Utils.php');
-require_once(__DIR__ . '/Token.php');
-require_once(__DIR__ . '/Database.php');
-require_once(__DIR__ . '/Todo.php');
-// require_once(__DIR__ . '/functions.php');
+// require_once(__DIR__ . '/Utils.php');
+// require_once(__DIR__ . '/Token.php');
+// require_once(__DIR__ . '/Database.php');
+// require_once(__DIR__ . '/Todo.php');
+
+spl_autoload_register(function ($class) {
+    $filename = __DIR__ . "/$class.php";
+    if (file_exists($filename)) {
+        require ($filename);
+    } else {
+        echo "file not found: $filename";
+        exit;
+    }
+});
