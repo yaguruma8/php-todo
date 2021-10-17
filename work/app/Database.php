5581cbb145fcc5150bc53695d035fa68d181d7fa
yaguruma <yaguruma_y@yahoo.co.jp>
2021-10-17 10:27:07 +0900

    Databaseクラスの作成

---
+++ work/app/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database
+{
+
+    private static $instance;
+
+    public static function getInstance()
+    {
+        if (isset(self::$instance)) {
+            return self::$instance;
+        }
+        try {
+            self::$instance = new PDO(
+                DSN,
+                DB_USER,
+                DB_PASS,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+                    PDO::ATTR_EMULATE_PREPARES => false,
+                ]
+            );
+            return self::$instance;
+        } catch (PDOException $e) {
+            $e->getMessage();
+            exit;
+        }
+    }
+
+}
--- work/app/functions.php
+++ work/app/functions.php
@@ -1,27 +1,5 @@
 <?php
 
-//
-// Database
-//
-function getPdoInstance()
-{
-    try {
-        $pdo = new PDO(
-            DSN,
-            DB_USER,
-            DB_PASS,
-            [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-                PDO::ATTR_EMULATE_PREPARES => false,
-            ]
-        );
-        return $pdo;
-    } catch (PDOException $e) {
-        $e->getMessage();
-        exit;
-    }
-}
 
 //
 // TODO関連
